630260b24a5285ba98ab50f1be0576a52d767d55
Ihor Honcharenko <89031570+IHORhoncharenko@users.noreply.github.com>
2023-10-21 01:52:45 +0300

    Add files via upload

--- lesson_8/js/script.js
+++ lesson_8/js/script.js
@@ -37,44 +37,44 @@
                         [...e['children']].forEach((el) => {
                             (el['localName'] === 'h2') && (`
                                 ${el['children'][0].addEventListener('click', () => {
-                                (el['nextElementSibling']['classList'].contains('properties__content')) && (`
+                                    (el['nextElementSibling']['classList'].contains('properties__content')) && (`
                                         ${el['nextElementSibling']['classList'].toggle('hide')}
                                         ${[...el['children']].forEach((ell) => {
-                                    (ell['localName'] === 'i') && (`
+                                            (ell['localName'] === 'i') && (`
                                                 ${(
-                                            () => {
-                                                var classNameEl = el['nextElementSibling']['className'];
+                                                    () => {
+                                                        var classNameEl = el['nextElementSibling']['className'];
 
-                                                switch (classNameEl) {
-                                                    case 'properties__content hide':
-                                                        (ell['nextElementSibling']['localName'] === 'svg') && (`
+                                                        switch (classNameEl) {
+                                                            case 'properties__content hide':
+                                                                (ell['nextElementSibling']['localName'] === 'svg') && (`
                                                                     ${ell['nextElementSibling'].remove()}
                                                                 `);
-                                                        ell.insertAdjacentHTML('afterend', `
-                                                                                    <svg>
-                                                                                        <use xlink:href="img/sprite.svg#arrow-up"></use>
-                                                                                    </svg>
-                                                                                    `)
-                                                        break;
-                                                    case 'properties__content':
-                                                        (ell['nextElementSibling']['localName'] === 'svg') && (`
-                                                                                    ${ell['nextElementSibling'].remove()}
-                                                                                `);
-                                                        ell.insertAdjacentHTML('afterend', `
-                                                                                    <svg>
-                                                                                        <use xlink:href="img/sprite.svg#arrow-down"></use>
-                                                                                    </svg>
-                                                                                `)
-                                                        break;
-                                                    default:
-                                                        break;
-                                                }
-                                            }
-                                        )()}
+                                                                ell.insertAdjacentHTML('afterend', `
+                                                                    <svg>
+                                                                        <use xlink:href="img/sprite.svg#arrow-up"></use>
+                                                                    </svg>
+                                                                `)
+                                                                break;
+                                                            case 'properties__content':
+                                                                (ell['nextElementSibling']['localName'] === 'svg') && (`
+                                                                    ${ell['nextElementSibling'].remove()}
+                                                                `);
+                                                                ell.insertAdjacentHTML('afterend', `
+                                                                    <svg>
+                                                                        <use xlink:href="img/sprite.svg#arrow-down"></use>
+                                                                    </svg>
+                                                                `)
+                                                                break;
+                                                            default:
+                                                                break;
+                                                        }
+                                                    }
+                                                )()}
                                             `)
-                                })}
+                                        })}
                                     `);
-                            })}
+                                })}
                             `);
                         });
                     });
@@ -87,8 +87,8 @@
                                     [...document.getElementsByTagName('select')].forEach((e) => {
                                         (e['dataset']['selected']) && (`
                                             ${e.addEventListener('click', () => {
-                                            e['dataset']['selected'] = e['value'];
-                                        })}
+                                                e['dataset']['selected'] = e['value'];
+                                            })}
                                         `);
                                     });
 
@@ -130,50 +130,50 @@
                             ${e['style']['color'] = `gray`};
                             ${e['style']['borderBottom'] = `2px solid gray`};
                             ${e.addEventListener('click', () => {
-                            e['defaultValue'] = '';
-                            e.addEventListener('input', (event) => {
-                                e['style']['minWidth'] = `${event['target']['value']['length']}ch`;
-                                styleText = e['value'];
-                                [...document.getElementById('background-prop')['children']].forEach((r) => {
-                                    [...r['children']].forEach((t) => {
-                                        (t['className'] === 'properties__result') && (`
-                                                ${t['style']['background'] = styleText}
+                                e['defaultValue'] = '';
+                                e.addEventListener('input', (event) => {
+                                    e['style']['minWidth'] = `${event['target']['value']['length']}ch`;
+                                    styleText = e['value'];
+                                    [...document.getElementById('background-prop')['children']].forEach((r) => {
+                                        [...r['children']].forEach((t) => {
+                                            (t['className'] === 'properties__result') && (`
+                                                    ${t['style']['background'] = styleText}
                                             `);
+                                        });
                                     });
                                 });
-                            })
-                        })}
+                            })}
                         `)
                     });
 
                     [...document.getElementsByClassName('emulation-group')[0]['children']].forEach((e) => {
                         (e['className'] === 'emulation-group__check') && (`
                             ${[...e['children'][0]['children']].forEach((el) => {
-                            (el['name'] === 'develop') && (`
+                                (el['name'] === 'develop') && (`
                                     ${el.addEventListener('change', (event) => {
-                                switch (event['target']['checked']) {
-                                    case true:
-                                        document.getElementsByClassName('emulation-group__emulation')[0]['style']['display'] = 'none';
-                                        document.getElementsByClassName('emulation-group__emulation-dev')[0]['style']['display'] = 'block';
-                                        break;
-                                    case false:
-                                        document.getElementsByClassName('emulation-group__emulation')[0]['style']['display'] = 'block';
-                                        document.getElementsByClassName('emulation-group__emulation-dev')[0]['style']['display'] = 'none';
-                                        break;
-                                    default:
-                                        break;
-                                }
-                            })}
+                                        switch (event['target']['checked']) {
+                                            case true:
+                                                document.getElementsByClassName('emulation-group__emulation')[0]['style']['display'] = 'none';
+                                                document.getElementsByClassName('emulation-group__emulation-dev')[0]['style']['display'] = 'block';
+                                                break;
+                                            case false:
+                                                document.getElementsByClassName('emulation-group__emulation')[0]['style']['display'] = 'block';
+                                                document.getElementsByClassName('emulation-group__emulation-dev')[0]['style']['display'] = 'none';
+                                                break;
+                                            default:
+                                                break;
+                                        }
+                                    })}
                                 `);
-                        })}
+                            })}
                         `);
                     });
 
                     [...document.getElementsByClassName('emulation-group__emulation-dev')[0]['children']].forEach((e) => {
                         (e['localName'] === 'svg') && (`
                             ${e.addEventListener('click', () => {
-                            e['classList'].toggle('modal');
-                        })}
+                                e['classList'].toggle('modal');
+                            })}
                         `);
                     });
                 }
